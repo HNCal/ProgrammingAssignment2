c36d3a17285a1886ac5335215b2459adb27730b3
hdnguyen2022 <hungdangnguyen@hotmail.com>
2022-04-11 22:19:15 -0700

    MOdify to get matrix and its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates an object that stores and makes accessible an
+## input matrix and its inverse for repeated usage
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates an object that includes 
+## input matrix
+## default inverse to null until it is recalled
+## list of functions to store and retrieve the matrix and its inverse
 
+makeCacheMatrix <- function(mat = matrix()) {
+  inver <- NULL
+  set <- function(y) {
+    mat <<- y
+    inver <<- NULL
+  }
+  get <- function() mat
+  setinverse <- function(inverse_mat) inver <<- inverse_mat
+  getinverse <- function() inver
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## verify whether the inverse exists
+## if not calculate the inverse and set to memory for future recalls
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mat, ...) {
+  inver <- mat$getinverse() #Get cached inverse
+  #if cached inverse is not null then retrieve value
+  if(!is.null(inver)) {
+    message("getting cached data")
+    return(inver)
+  }
+  data <- mat$get() # get input matrix as 'data'
+  inver <- solve(data, ...) #solve inverse of input matrix
+  mat$setinverse(inver) #cache calculated inverse
+  inver
+}
         ## Return a matrix that is the inverse of 'x'
-}
